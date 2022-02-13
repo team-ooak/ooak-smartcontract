2c71956b8ea9196665d80b457e641cf100320542
Woojinger <tlsdnwls0511@naver.com>
2022-02-13 21:56:53 +0900

    implement mint and transfer

--- contracts/OoakMinting.sol
+++ contracts/OoakMinting.sol
@@ -4,42 +4,82 @@ pragma solidity ^0.5.0;
 
 import "./ownership/Ownable.sol";
 
+import "./token/KIP17/KIP17Token.sol";
+
 contract OoakMinting is Ownable {
-    // twitch ID to nameTag
-    mapping(string => string) IdToNameTag;
     // twitch ID to public address
     mapping(string => address) IdToAddress;
+
+    mapping(address => uint256) AddressToTokenId;
+    // NFT TokenId to NameTag
+    mapping(uint256 => string) TokenIdToNameTag;
     // twitch ID(streamer) to public address
     mapping(string => address) StreamerToAddress;
-    // address to tokenURI
-    mapping(address => string) AddressToTokenURI;
+    // TokenId to tokenURI
+    mapping(uint256 => string) TokenIdToTokenURI;
     // check nameTag already exist
     mapping(string => bool) isNameTagExist; 
 
+    uint256 public lastTokenId;
+    address NftContract;
+
+    constructor() public {
+        lastTokenId = 0;
+    }
+
     function registerNameTag(string memory twitchId, address publicKey, string memory nameTag) onlyOwner public {
-        
-        require(bytes(IdToNameTag[twitchId]).length == 0, "already register nameTag");
+        require(bytes(getNameTag(twitchId)).length == 0, "already register nameTag");
         require(!isNameTagExist[nameTag], "this nameTag already exist");
-        IdToNameTag[twitchId]=nameTag;
-        IdToAddress[twitchId]=publicKey;
-        isNameTagExist[nameTag]=true;
-    
+
+        lastTokenId++;
+
+        IdToAddress[twitchId] = publicKey;
+        AddressToTokenId[publicKey] = lastTokenId;
+        TokenIdToNameTag[lastTokenId] = nameTag;
+        isNameTagExist[nameTag] = true;
     }
 
     function getNameTag(string memory twitchId) public view returns (string memory) {
-        return IdToNameTag[twitchId];
+        uint256 _tokenId = getTokenId(twitchId);
+        return getNameTagWithTokenId(_tokenId);
+    }
+
+    function getNameTagWithTokenId(uint256 tokenId) public view returns (string memory) {
+        return TokenIdToNameTag[tokenId];
     }
 
     function getAddress(string memory twitchId) public view returns (address) {
         return IdToAddress[twitchId];
     }
 
-    function getStreamerAddress(string memory twitchId) public view returns (address) {
-        return StreamerToAddress[twitchId];
+    function getTokenURI(string memory twitchId) public view returns (string memory) {
+        require(IdToAddress[twitchId] != address(0), "address do not exist");
+        address _address = IdToAddress[twitchId];
+        uint256 _tokenId = AddressToTokenId[_address];
+        return TokenIdToTokenURI[_tokenId];
     }
 
-    function getTokenURI(address publicKey) public view returns (string memory) {
-        return AddressToTokenURI[publicKey];
+    function getTokenId(string memory twitchId) public view returns (uint256) {
+        address _address = IdToAddress[twitchId];
+        return AddressToTokenId[_address];
+    }
+    
+    function mintNFTWithTokenURI(string memory twitchId, string memory tokenURI) onlyOwner public {
+        require(IdToAddress[twitchId] != address(0), "address do not exist");
+        address publicKey = IdToAddress[twitchId];
+        uint256 tokenId = AddressToTokenId[publicKey];
+        KIP17Token(NftContract).mintWithTokenURI(publicKey, tokenId, tokenURI);
+        TokenIdToTokenURI[tokenId] = tokenURI;
     }
 
+    function setNFTContract(address nftContract) onlyOwner public {
+        NftContract = nftContract;
+    }
+
+    function changeOwnerOfNameTag(address srcAddress, address dstAddress, uint256 tokenId) public {
+        require(msg.sender == NftContract);
+        delete AddressToTokenId[srcAddress];
+        AddressToTokenId[dstAddress] = tokenId;
+    }
 }
+
--- test/ooak/minting_test.js
+++ test/ooak/minting_test.js
@@ -2,12 +2,13 @@ const { assert } = require('chai');
 const shouldFail = require('../helpers/shouldFail');
 
 const OoakMinting = artifacts.require('OoakMinting.sol');
+const OoakNFT = artifacts.require('OoakNFT.sol')
 
 var mintContract
+var nftContract
 var addresses
 contract('Score Test', function (accounts) {
     before(async function () {
-        mintContract = await OoakMinting.new({ from : accounts[0] });
         addresses = [
             "0x33ebcd60ce9291674bafE4cDfe156E976e39de88",
             "0x78a17cd56BE4E7b06a07Dc2823BAea29B7dDc650",
@@ -16,6 +17,12 @@ contract('Score Test', function (accounts) {
             "0x12756876B8c3b37eF967fd4146fBA031f243395a",
             "0xCEe5CE3F45f2AD4AA6d637a7Cf94542518ecf79b",
         ]
+
+        mintContract = await OoakMinting.new({ from : accounts[0] });
+        nftContract = await OoakNFT.new({ from : accounts[0]} );
+        await nftContract.setOoakContract(mintContract.address, {from : accounts[0]});
+        await mintContract.setNFTContract(nftContract.address, {from : accounts[0]});
+        await nftContract.addMinter(mintContract.address, {from : accounts[0]});
     })
 
     describe('Register NameTag', function () {
@@ -23,8 +30,6 @@ contract('Score Test', function (accounts) {
             await mintContract.registerNameTag("twitchId1", addresses[0], "nameTag1", {from: accounts[0]})
             await mintContract.registerNameTag("twitchId2", addresses[1], "nameTag2")
             await mintContract.registerNameTag("twitchId3", addresses[2], "nameTag3")
-            assert.equal(await mintContract.getNameTag("twitchId1"), "nameTag1")
-
         })
         it("test should fail : this nameTag already exist", async function () {
             await shouldFail.reverting.withMessage(
@@ -39,15 +44,38 @@ contract('Score Test', function (accounts) {
                 mintContract.registerNameTag("twitchId6",addresses[5],"nameTag6",{from: accounts[1]}));
         })
     })
-    describe('Change score contract to ScoreV2', function () {
-        it("deploy ScoreV2 contract", async function () {
-  
-        })
-        it("test for accounts1", async function () {
-            
-        })
-        it("test for accounts2", async function () {
-            
+    describe('get NameTag', function () {
+        it("get NameTag : twitchId1, twitchId2, twitchId3", async function () {
+            assert.equal(await mintContract.getNameTag("twitchId1"), "nameTag1")
+            assert.equal(await mintContract.getNameTag("twitchId2"), "nameTag2")
+            assert.equal(await mintContract.getNameTag("twitchId3"), "nameTag3")
         })
     })
+    describe('NFT', function () {
+        it("mint", async function () {
+            sampleId = "twitchId1"
+            sampleURI = "URI1"
+
+            await mintContract.mintNFTWithTokenURI(sampleId, sampleURI, {from: accounts[0]});
+            _address = await mintContract.getAddress(sampleId)
+            _tokenId = await mintContract.getTokenId(sampleId);
+            _ownerAddress = await nftContract.ownerOf(_tokenId);
+            _URI = await nftContract.tokenURI(_tokenId);
+            assert.equal(_address, _ownerAddress)
+            assert.equal(sampleURI, _URI);
+        })
+        it("transfer NFT : twitchId1 to twitchId2", async function () {
+            _address1 = await mintContract.getAddress("twitchId1");
+            _address2 = await mintContract.getAddress("twitchId2");
+            _tokenId = await mintContract.getTokenId("twitchId1");
+            await nftContract.safeTransferFrom(_address1, _address2, _tokenId.toNumber());
+            assert.equal(await mintContract.getTokenId("twitchId2"), _tokenId.toNumber());
+            assert.equal(await mintContract.getNameTagWithTokenId(_tokenId.toNumber()), "nameTag1");
+        })
+        it("getNameTag", async function () {
+            assert.equal(await mintContract.getNameTag("twitchId2"), "nameTag1");
+            assert.notEqual(await mintContract.getNameTag("twitchId1"), "nameTag1");
+        }) 
+    })
+
 })
