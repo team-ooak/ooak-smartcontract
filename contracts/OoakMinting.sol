e8f30ac4718191ff0748e628bfbda76fe5882a7c
ybuser <ybuser@naver.com>
2022-02-17 22:23:34 +0900

    wallet - ID tag 1:N

--- contracts/OoakMinting.sol
+++ contracts/OoakMinting.sol
@@ -7,10 +7,17 @@ import "./ownership/Ownable.sol";
 import "./token/KIP17/KIP17Token.sol";
 
 contract OoakMinting is Ownable {
+
+
     // twitch ID to public address
     mapping(string => address) IdToAddress;
+
+
     // TODO : 여러 개의 NFT를 가지도록
-    mapping(address => uint256) AddressToTokenId;
+    //mapping(address => uint256) AddressToTokenId;
+    mapping(address => uint256[]) AddressToTokenIds;
+
+
     // NFT TokenId to NameTag
     mapping(uint256 => string) TokenIdToNameTag;
     // twitch ID(streamer) to public address
@@ -36,14 +43,22 @@ contract OoakMinting is Ownable {
         lastTokenId++;
 
         IdToAddress[twitchId] = publicKey;
-        AddressToTokenId[publicKey] = lastTokenId;
+        //AddressToTokenId[publicKey] = lastTokenId;
+        AddressToTokenIds[publicKey].push(lastTokenId);
         TokenIdToNameTag[lastTokenId] = nameTag;
         isNameTagExist[nameTag] = true;
     }
 
-    function getNameTag(string memory twitchId) public view returns (string memory) {
-        uint256 _tokenId = getTokenId(twitchId);
-        return getNameTagWithTokenId(_tokenId);
+    // function getNameTag(string memory twitchId) public view returns (string memory) {
+    //     uint256 _tokenId = getTokenId(twitchId);
+    //     return getNameTagWithTokenId(_tokenId);
+    // }
+    function getNameTags(string memory twitchId) public view returns (string[] memory) {
+        uint256[] _tokenIds = getTokenIds(twitchId);
+        string[] _nameTags;
+        for (uint i=0; i<_tokenIds.length; i++) _nameTags.push(getNameTagWithTokenId(_tokenIds[i]));
+        
+        return _nameTags;
     }
 
     function getNameTagWithTokenId(uint256 tokenId) public view returns (string memory) {
@@ -54,34 +69,79 @@ contract OoakMinting is Ownable {
         return IdToAddress[twitchId];
     }
 
-    function getTokenURI(string memory twitchId) public view returns (string memory) {
+    // function getTokenURI(string memory twitchId) public view returns (string memory) {
+    //     require(IdToAddress[twitchId] != address(0), "address do not exist");
+    //     address _address = IdToAddress[twitchId];
+    //     uint256 _tokenId = AddressToTokenId[_address];
+    //     return TokenIdToTokenURI[_tokenId];
+    // }
+    function getTokenURIs(string memory twitchId) public view returns (string[] memory) {
         require(IdToAddress[twitchId] != address(0), "address do not exist");
         address _address = IdToAddress[twitchId];
-        uint256 _tokenId = AddressToTokenId[_address];
-        return TokenIdToTokenURI[_tokenId];
+        uint256[] _tokenIds = AddressToTokenIds[_address];
+        string[] _tokenURIs;
+        for (uint i=0; i<_tokenIds.length; i++) _tokenURIs.push(TokenIdToTokenURI[_tokenId[i]]);
+        return _tokenURIs;
     }
 
-    function getTokenId(string memory twitchId) public view returns (uint256) {
+    // function getTokenId(string memory twitchId) public view returns (uint256) {
+    //     address _address = IdToAddress[twitchId];
+    //     return AddressToTokenId[_address];
+    // }
+    function getTokenIds(string memory twitchId) public view returns (uint256[]) {
         address _address = IdToAddress[twitchId];
-        return AddressToTokenId[_address];
+        return AddressToTokenIds[_address];
     }
     
+    // function mintNFTWithTokenURI(string memory twitchId, string memory tokenURI) onlyOwner public {
+    //     require(IdToAddress[twitchId] != address(0), "address do not exist");
+    //     address publicKey = IdToAddress[twitchId];
+    //     uint256 tokenId = AddressToTokenId[publicKey];
+    //     KIP17Token(NftContract).mintWithTokenURI(publicKey, tokenId, tokenURI);
+    //     TokenIdToTokenURI[tokenId] = tokenURI;
+    // }
+
+    // TODO 확인 필요
     function mintNFTWithTokenURI(string memory twitchId, string memory tokenURI) onlyOwner public {
         require(IdToAddress[twitchId] != address(0), "address do not exist");
         address publicKey = IdToAddress[twitchId];
-        uint256 tokenId = AddressToTokenId[publicKey];
-        KIP17Token(NftContract).mintWithTokenURI(publicKey, tokenId, tokenURI);
+        uint256[] tokenIds = AddressToTokenIds[publicKey];
+        KIP17Token(NftContract).mintWithTokenURI(publicKey, tokenIds[tokenIds.length-1], tokenURI); //tokenIds 중 마지막 tokenId 이용
         TokenIdToTokenURI[tokenId] = tokenURI;
     }
+
 
     function setNFTContract(address nftContract) onlyOwner public {
         NftContract = nftContract;
     }
-
+    
+    
+    // function changeOwnerOfNameTag(address srcAddress, address dstAddress, uint256 tokenId) public {
+    //     require(msg.sender == NftContract);
+    //     delete AddressToTokenId[srcAddress];
+    //     AddressToTokenId[dstAddress] = tokenId;
+    // }
     function changeOwnerOfNameTag(address srcAddress, address dstAddress, uint256 tokenId) public {
         require(msg.sender == NftContract);
-        delete AddressToTokenId[srcAddress];
-        AddressToTokenId[dstAddress] = tokenId;
+        
+        //tokenId가 있는 자리 delete
+        uint deletedPlace = 0;
+        for(uint i=0; i<AddressToTokenId[srcAddress].length; i++){
+            if(AddressToTokenId[srcAddress][i] == tokenId){
+                delete AddressToTokenId[srcAddress][i];
+                deletedPlace = i;
+                break;
+            }
+        }
+        //delete 한 자리 뒤 쪽 앞으로 당기기
+        for(uint i=deletedPlace; i<AddressToTokenId[srcAddress].length-1; i++){
+            
+            AddressToTokenId[srcAddress][i] = AddressToTokenId[srcAddress][i+1];
+        }
+        AddressToTokenId[srcAddress].pop;
+
+        //dstAddress 뒤에 tokenId 추가
+        AddressToTokenIds[dstAddress][AddressToTokenIds[dstAddress].length] = tokenId;
     }
 }
 
